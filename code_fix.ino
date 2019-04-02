67609254d1c92eaf93eae70824876e62037b26d8
2black0 <2black0@gmail.com>
2019-04-02 15:16:47 +0700

    Add files via upload

---
+++ code_fix.ino
@@ -0,0 +1,163 @@
+//library used
+#include <ESP8266WiFi.h>
+#include <time.h>
+#include <SimpleDHT.h>
+#include "ThingSpeak.h"
+
+//dht configuration
+#define DHT_PIN D4
+SimpleDHT11 dht11(DHT_PIN);
+byte temperature = 0;
+byte humidity = 0;
+
+//rainfall configuration
+#define interruptPin D1 
+volatile unsigned long contactTime;
+const double bucketSize = 1.346;
+float dailyRain = 0.0;          
+float hourlyRain = 0.0;        
+float dailyRain_till_LastHour = 0.0;
+bool first;  
+
+//wifi configuration
+const char* ssid = "WIFI_GRATIS";
+const char* pass = "arDY1234*$";
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+//time configuration
+int timezone = 7 * 3600;
+int dst = 0;
+
+//thingspeak configuration
+unsigned long myChannelNumber = 746681;
+const char *myWriteAPIKey = "TLO5WA41KTENXZ1Y";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+  delay(10);
+  
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //Serial.print(".");
+  } 
+  
+  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
+  while(!time(nullptr)){
+     Serial.print("*");
+     delay(1000);
+  }
+
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+  
+  Serial.println(WiFi.localIP());
+  Serial.println("Weather Information");
+  Serial.println("--------------------");
+}
+
+void thingspeak_send(){
+    // set the fields with the values
+  ThingSpeak.setField(1, temperature);
+  ThingSpeak.setField(2, humidity);
+  ThingSpeak.setField(3, hourlyRain);
+  ThingSpeak.setField(4, dailyRain);
+  
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+}
+ 
+void handleInterrupt() {
+  if((millis() - contactTime) > 15){
+    dailyRain+=bucketSize;
+    contactTime = millis();
+  }
+}
+
+void rain_daily(){
+  time_t now = time(nullptr);
+  struct tm * timeinfo;
+  timeinfo = gmtime(&now);
+  
+  //Serial.println("--------------");
+  //Serial.print("Hour = ");Serial.println(timeinfo->tm_hour);
+  //Serial.print("Min = ");Serial.println(timeinfo->tm_min); 
+  //Serial.print("Sec = ");Serial.println(timeinfo->tm_sec); 
+  //Serial.println("--------------");
+}
+
+void counterRain(){
+  time_t now = time(nullptr);
+  struct tm * timeinfo;
+  timeinfo = gmtime(&now);
+  //Serial.print(timeinfo->tm_hour);
+  //Serial.print(timeinfo->tm_min); 
+  if(timeinfo->tm_min != 0) first = true;
+  if(timeinfo->tm_min == 0 && first == true){
+    hourlyRain = dailyRain - dailyRain_till_LastHour;
+    dailyRain_till_LastHour = hourlyRain;
+    first = false;
+  }
+  if(timeinfo->tm_hour == 0){
+    dailyRain = 0.0;
+    dailyRain_till_LastHour = 0.0;
+  }
+}
+
+void showData(){
+  time_t now = time(nullptr);
+  Serial.print("Date and Time Now = ");
+  Serial.println(ctime(&now));
+  Serial.print("Air Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+  Serial.print("Air Humidity = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  Serial.print("Rainfall in last Hour = ");
+  Serial.print(hourlyRain,8);
+  Serial.println(" mm");
+  Serial.print("Rainfall Daily = ");
+  Serial.print(dailyRain,8);
+  Serial.println(" mm");
+  Serial.println("--------------------");
+}
+
+void getSensor(){
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    //Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+}
+ 
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+  
+  getSensor();
+  counterRain();
+  //rain_daily();
+  showData();
+  thingspeak_send();
+  delay(20000);
+
+}
